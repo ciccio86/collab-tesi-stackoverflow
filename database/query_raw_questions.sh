f3e1971744029a2f2de751235d36d57304388396
Francesco Pavone <ciccio1986@gmail.com>
2015-08-28 16:31:04 +0200

    Added time logging.

--- database/query_raw_questions.sh
+++ database/query_raw_questions.sh
@@ -4,26 +4,38 @@ OLD_PWD=$PWD
 cd ${0%/*}
 DIR="./datasets"
 TEMP_DIR="/tmp"
-CREATION_TIMESTAMP="$(date +%s%N)"
+LOG_DIR="./logs"
+CREATION_TIMESTAMP="$(date +%s)"
 FILENAME="raw_questions_$CREATION_TIMESTAMP.csv"
 TEMP_FILE="/tmp/raw_questions.csv"
 FILE="$DIR/$FILENAME"
+LOGFILE="$LOG_DIR/log_$CREATION_TIMESTAMP.log"
+ERRORFILE="$LOG_DIR/errors_$CREATION_TIMESTAMP.log"
 
 echo
-echo "Preparing dataset folder . . ."
+echo "Preparing dataset and logs folder . . ."
 
 mkdir -p $DIR
+mkdir -p $LOG_DIR
+
+echo
+echo "Removing old temp file . . ."
+
+rm -f /tmp/raw_questions.csv
 
 echo
 echo "Creating csv raw dataset in temp folder . . ."
 
-mysql -D stackoverflow_march < query_raw_questions.sql #-e "set @temp_file='$TEMP_FILE'; source query_raw_questions.sql;" #TODO: log time
+printf "Query execution times.\n======================\n" >> $LOGFILE
+{ time mysql -D stackoverflow_march < query_raw_questions.sql 2>> $ERRORFILE ; } 2>> $LOGFILE
 
 echo
 echo "Moving dataset to file $FILE . . ."
 
-mv $TEMP_FILE $FILE #TODO: log time
+printf "\nMoving file times.\n==================\n" >> $LOGFILE
+{ time mv $TEMP_FILE $FILE 2>> $ERRORFILE ; } 2>> $LOGFILE
 
+#TODO: do not show if any error
 echo
 echo "CSV dataset successfully exported!"
 

cc2d7ecde20f73d67b58234878bd02ecfeae8eb1
Francesco Pavone <ciccio1986@gmail.com>
2015-09-02 10:14:21 +0200

    added sample raw_dataset. added script to filter valid questions

---
+++ stackexchange-metrics-calculator/build.sbt
@@ -0,0 +1,11 @@
+name := """stackexchange-metrics-calculator"""
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.3.11",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.11" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+  "com.github.tototoshi" %% "scala-csv" % "1.2.2")
---
+++ stackexchange-metrics-calculator/src/main/scala/it/uniba/di/collab/stackexchange/scripts/filterQuestionIds.scala
@@ -0,0 +1,86 @@
+/**
+ * Created by francesco on 01/09/15.
+ */
+package it.uniba.di.collab.stackexchange.scripts
+
+import com.github.tototoshi.csv._
+import java.io._
+
+object filterQuestionIds {
+  def main(args: Array[String]) {
+
+    if (args.length > 1) {
+      val validQuestionFilePath = args(0)
+      val rawQuestionsFilePath = args(1)
+      val outputFilePath = args(2)
+
+      val nonExistentFiles = Nil
+      if (!new java.io.File(validQuestionFilePath).exists)
+        nonExistentFiles :+ validQuestionFilePath
+      if (!new java.io.File(rawQuestionsFilePath).exists)
+        nonExistentFiles :+ rawQuestionsFilePath
+      if (!new java.io.File(outputFilePath).exists)
+        nonExistentFiles :+ outputFilePath
+
+      if (nonExistentFiles.isEmpty) {
+
+        //TODO: try
+        try {
+          val ids: List[Int] = getValidIds(validQuestionFilePath)
+          createFilteredCSV(rawQuestionsFilePath, outputFilePath, ids)
+        } catch {
+          case e: Exception => e.printStackTrace()
+        }
+
+      } else {
+        println("The following files do not exist:\n")
+        nonExistentFiles.foreach(println)
+        System.exit(1)
+      }
+
+    } else {
+      println("Please specify the path of the file containing di ids of the valid questions as the first parameter and the raw questions file as the second parameter.")
+      System.exit(1)
+    }
+  }
+
+  def getValidIds(filePath: String): List[Int] = {
+    implicit object format extends DefaultCSVFormat {
+      override val delimiter: Char = ';'
+    }
+
+    val reader = CSVReader.open(new File(filePath))(format)
+    try {
+      val iterator = reader.iteratorWithHeaders
+      iterator.map(elem => elem("PostId").toInt).toList
+    } catch {
+      case e: Exception => throw e
+    } finally {
+      reader.close()
+    }
+
+  }
+
+  def createFilteredCSV(filePath: String, outputFilePath: String, ids: List[Int]): Unit = {
+    implicit object format extends DefaultCSVFormat {
+      override val delimiter: Char = ';'
+      override val quoteChar: Char = '"'
+      override val escapeChar: Char = '\\'
+      override val lineTerminator: String = "\n"
+    }
+
+    val writer = CSVWriter.open(new File(outputFilePath))(format)
+
+    val reader = CSVReader.open(new File(filePath))(format)
+
+    try {
+      val filteredRows = reader.all().tail.filter(fields => ids.contains(fields.head.toInt))
+      writer.writeAll(filteredRows)
+    } catch {
+      case e: Exception => throw e
+    } finally {
+      writer.close()
+      reader.close()
+    }
+  }
+}
